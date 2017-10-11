16c5be57ba84dc9131b10769526e44ce150af3f8
Grant Yang <grantyang1@gmail.com>
2017-10-11 17:07:30 +0200

    profile with authentication

--- src/components/profile.js
+++ src/components/profile.js
@@ -5,7 +5,38 @@ import { Link } from 'react-router-dom';
 class Profile extends Component {
   constructor(props) {
     super(props);
+    this.state ={
+      name: '',
+      email: ''
+    };
   }
+
+  //I am logged in, so i have a cookie with authId
+  //I send the server authId in the cookie when i want data
+  //Server checks to see if authId matches a current session (middleware?)
+  //If the authId i sent matches a session, server sends me data.
+  //If no match, send error status code.
+  
+  componentDidMount() {
+
+    fetch(`http://localhost:5000/profile/`, {
+			method: 'GET',
+			    credentials: 'include'
+		})
+			.then(res => {
+        if (res.status === 403) return alert('Please Log In')                      
+        if (res.status === 401) return alert('Invalid Token')              
+				return res.json();
+			})
+			.then(returnedUser => {
+        if (returnedUser){
+          this.setState({
+            name: returnedUser.name,
+            email: returnedUser.email
+          });
+        }
+			});
+	}
 
   render() {
     return (
@@ -13,10 +44,10 @@ class Profile extends Component {
         <NavBar/>
         <div className="container mt-2">
           <div className="justify-content-sm-left row">
-            <span>Name</span>
+            <span>{this.state.name}</span>
           </div>
           <div className="justify-content-sm-left row">
-            <span>Email address</span>
+            <span>{this.state.email}</span>
           </div>
           <div className="row">
             <Link
