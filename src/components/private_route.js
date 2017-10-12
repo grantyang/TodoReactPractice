4bf7846bf2bbcf25a3ad01f4ec17cd8e5b500f04
Grant Yang <grantyang1@gmail.com>
2017-10-12 19:14:52 +0200

    user can only see their own lists

--- src/components/private_route.js
+++ src/components/private_route.js
@@ -7,34 +7,29 @@ import {
   withRouter
 } from 'react-router-dom'
 
-// const isAuthenticated = () => { 
-//   fetch('http://localhost:5000/user',{
-//   method: 'GET',
-//   credentials: 'include'
-// })
-//   .then(res => {
-//     console.log(`authenticated 1`)      
-//     if (res.status===403) { //if error code is returned, then user is not logged in
-//       console.log(`authenticated 1.5`)            
-//       return 
-//     }
-//     console.log(`authenticated 1.75`)          
-//     return res.json();
-//   })
-//   .then(user => {
-//     if (user) { //otherwise, if there is a valid user logged in
-//       console.log(`authenticated 2`)      
-//       return true
-//     }
-//     console.log(`authenticated 3`)      
-//     alert('Please log in to view that page')
-//     return false
-//   })
-// }
+const isAuthenticated = () => { 
+  fetch('http://localhost:5000/user',{
+  method: 'GET',
+  credentials: 'include'
+})
+  .then(res => {
+    if (res.status===403) { //if error code is returned, then user is not logged in
+      return 
+    }
+    return res.json();
+  })
+  .then(user => {
+    if (user) { //otherwise, if there is a valid user logged in
+      return true
+    }
+    alert('Please log in to view that page')
+    return false
+  })
+}
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={props => ( // what is this render={props => ... } doing? what is set as props
-      document.cookie ? ( //GY !!!!!!!
+      isAuthenticated ? ( //GY !!!!!!!
         <Component {...props}/>
         
       ) : (
