c47ebc25751856b8756be2a4341657dab2401bee
Grant Yang <grantyang1@gmail.com>
2017-10-12 14:04:20 +0200

    homepage changes view based on active session

--- src/App.js
+++ src/App.js
@@ -5,32 +5,67 @@ import Input from './components/input.js';
 import ListOfLists from './presentational/list_of_lists.js';
 
 class App extends Component {
+//   fetch(`http://localhost:5000/user/`, {
+//     method: 'GET',
+//         credentials: 'include'
+//   })
+//     .then(res => {
+//       if (res.status === 403) return alert('Please Log In')                      
+//       if (res.status === 401) return alert('Invalid Token')              
+//       return res.json();
+//     })
+//     .then(returnedUser => {
+//       if (returnedUser){
+//         this.setState({
+//           name: returnedUser.name,
+//           email: returnedUser.email
+//         });
+//       }
+//     });
+// }
   componentDidMount() {
-    
-    fetch('http://localhost:5000/lists/', {
+    fetch('http://localhost:5000/user', {
       method: 'GET',
       credentials: 'include'
     })
       .then(res => {
+        if (res.status===403) {
+          this.setState({
+            currentUser: null,
+            loading: false            
+          });
+          return null
+        }
         return res.json();
       })
-      .then(returnedLists => {
-        this.setState({
-          listOfLists: returnedLists, // load in initial lists from server
-          loading: false
-        });
+      .then(user => {
+        if (user) {
+          this.setState({
+            currentUser: user
+          });
+          fetch('http://localhost:5000/lists/', {
+            method: 'GET',
+            credentials: 'include'
+          })
+            .then(res => {
+              return res.json();
+            })
+            .then(returnedLists => {
+              this.setState({
+                listOfLists: returnedLists, // load in initial lists from server
+                loading: false
+              });
+            });
+          }
       });
-
-
-      }
-  
+  }
 
   constructor(props) {
     super(props);
     this.state = {
       listOfLists: [],
       loading: true,
-      currentUser: {}
+      currentUser: null
     };
   }
 
@@ -40,7 +75,11 @@ class App extends Component {
       return;
     }
 
-    if (this.state.listOfLists.find(item => item.name.toLowerCase() === newName.toLowerCase())) {
+    if (
+      this.state.listOfLists.find(
+        item => item.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
       //if there is a duplicate
       alert('Todo List already exists.');
       return;
@@ -73,21 +112,21 @@ class App extends Component {
       });
   };
 
-  render() {    
+  render() {
     return (
       <div className="List">
         <NavBar />
         {!this.state.currentUser && <h1>Please log in to view this page.</h1>}
-        {this.state.currentUser &&
-        <div className="container">
-          
-          <Input fxToRun={this.create} />
-          <ListOfLists
-            className=""
-            listOfLists={this.state.listOfLists}
-            loading={this.state.loading}
-          />
-        </div>}
+        {this.state.currentUser && (
+          <div className="container">
+            <Input fxToRun={this.create} />
+            <ListOfLists
+              className=""
+              listOfLists={this.state.listOfLists}
+              loading={this.state.loading}
+            />
+          </div>
+        )}
       </div>
     );
   }
