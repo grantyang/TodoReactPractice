d3120760c28e19843829c8a73d347ab475fa686c
Grant Yang <grantyang1@gmail.com>
2017-10-30 03:21:03 +0100

    about page photo

--- src/presentational/about.js
+++ src/presentational/about.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from '../containers/nav_bar.js';
+import pusheen from '../pusheen.jpg'
 
 const About = props => {
   return (
     <div>
       <NavBar/>
-      <div className="row justify-content-center mt-2">
+      <div className="row justify-content-center mt-4">
         <span>
           This todo app was created by <b>Grant Yang</b>
         </span>
@@ -13,8 +14,8 @@ const About = props => {
       <div className="row justify-content-center">
         <span>Please contact me at grantyang1@gmail.com. Thank you!</span>
       </div>
-      <div className="row justify-content-center">
-        <h1>pic goes here</h1>
+      <div className="row justify-content-center mt-4">
+      <img className= "pusheen rounded" src={pusheen} alt={"me_irl"}/> 
       </div>
 
     </div>
@@ -22,3 +23,4 @@ const About = props => {
 };
 
 export default About;
+
